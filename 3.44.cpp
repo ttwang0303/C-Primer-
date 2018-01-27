413fe56abda55d5f4b8015cae3e0522c39413845
ttwang0303 <1551662579@qq.com>
2018-01-27 19:57:37 +0800

    C++ Primer练习3.44答案

---
+++ 3.44.cpp
@@ -0,0 +1,38 @@
+/*题目要求：编写3个不同版本的程序，令其均能输出ia的元素。
+            版本1使用范围for语句管理迭代过程；版本2和3都使用普通的for语句，
+            其中版本2要求用下标运算符，版本3要求用指针。另外，在所有3个版本的程序中都要直接写出数据类型，
+            改写3.43程序，使用类型别名来代替循环控制变量的类型          */
+#include<iostream>
+
+using namespace std;
+using int_array = int[4];
+
+int main()
+{
+  int ia[3][4] = {0,1,2,3,4,5,6,7,8,9,10,11};
+  cout << "利用范围for语句输出多维数组的内容：" << endl;
+  for(int_array &row : ia)
+  {
+     for(int &col : row)
+	     cout << col << " ";
+     cout << endl;
+  }
+
+  cout << "利用普通for语句和下标运算符号输出多维数组的内容： "  << endl;
+  for(int i = 0; i != 3; i++)
+  {
+     for(int j = 0; j != 4; j++)
+	     cout << ia[i][j] << " ";
+     cout << endl;
+  }
+
+  cout << "利用普通for语句和指针输出多维数组的内容： " << endl;
+  for(int_array *p = ia ; p != ia + 3;  p++)
+  {
+    for(int *q = *p; q != *p + 4 ; q++)
+	    cout << *q << " ";
+    cout << endl;
+  }
+
+  return 0;
+}
